56143d1935a39696e715436bcd1a1de78cca0340
Christian Meyer <chritsch@gmail.com>
2023-12-08 09:56:50 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,108 @@
+import datetime as dt
+import os
+import shutil
+
+from rich.console import Console
+
+DOWNLOADS_PATH = os.path.expanduser('~\Downloads')  # Pfad zu Downloads
+
+# Prüft ob Datei älter als n Tage ist
+def old_file(file, days=3):
+    if dt.datetime.now() - dt.datetime.fromtimestamp(os.path.getctime(file)) > dt.timedelta(days):
+        return True
+
+
+# Main
+def main():
+    console = Console()
+    working_path = DOWNLOADS_PATH
+    sortbydate: bool = False
+    showoldfiles: bool = False
+    oldfilesdays: int = 3
+
+    while True:
+        os.system('cls')
+        os.chdir(working_path)
+        console.print(f'Path: [blue]{os.getcwd()}[/blue]\n')
+
+        # Gibt alle Ordner blau aus und fügt sie einer Liste hinzu
+        directories = []
+        for data in os.listdir():
+            if os.path.isdir(data):
+                directories.append(data)
+                console.print(f'{data}', style='blue')
+
+        # Fügt Dateien einer Liste hinzu
+        files = []
+        for data in os.listdir():
+            if os.path.isfile(data):
+                files.append(data)
+
+        # Bool: Sortiert Dateien nach Datum
+        if sortbydate:
+            files.sort(key=os.path.getctime, reverse=True)
+
+        for file in files:
+            # Bool: Markiert Dateien, die älter als n Tage sind
+            if showoldfiles:
+                if old_file(file, oldfilesdays):
+                    console.print(f'{file}', style='red')
+                else:
+                    print(f'{file}')
+            else:
+                print(f'{file}')
+
+        # Gibt Befehle aus
+        print()
+        console.print(f'Commands:', style='bold')
+
+        console.print(f'{"sort":15}Sort by date: {sortbydate}')
+        console.print(f'{"mark":15}Mark files older than {oldfilesdays} days: {showoldfiles}')
+        print(f'{"open [name]":15}Open folder')
+        print(f'{"up":15}Open the parent directory')
+        print(f'{"reset":15}Reset path to Downloads')
+
+        print(f'{"":15}---')
+        print(f'{"del [name]":15}Delete file or folder')  # TODO: Implement delete file or folder
+        console.print(f'{"del old":15}Delete files older than {oldfilesdays} days')  # TODO: Implement delete old files
+        print(f'{"del all":15}Delete all files')  # TODO: Implement delete all files
+
+        print(f'{"":15}---')
+        print(f'{"quit":15}Quit')
+
+        # Führt Eingabe aus
+        cmd = console.input('\n[bold]cmd >>[/bold] ').lower()
+        if cmd in ['quit', 'q']:  # Beenden
+            break
+        elif cmd in ['sort', 's']:  # Sortiere Dateien nach Datum
+            sortbydate = not sortbydate
+        elif cmd in ['mark', 'm']:  # Markiere alte Dateien
+            showoldfiles = not showoldfiles
+        elif cmd in ['up', 'u']:  # Öffne übergeordneten Ordner
+            os.chdir('..')
+            working_path = os.getcwd()
+        elif cmd.split()[0] in ['open', 'o']:  # Öffne Ordner
+            # Prüft, ob Ordnername existiert
+            try:
+                os.chdir(working_path + '\\' + cmd.split()[1])
+            except Exception:
+                pass
+            # Prüft ob Eingabe gültige Zahl ist und weisst Ordner zu
+            try:
+                if cmd.split()[1].isdigit():
+                    os.chdir(working_path + '\\' + directories[int(cmd.split()[1]) - 1])
+            except Exception:
+                pass
+            # Wenn True, dann öffne Ordner
+            working_path = os.getcwd()
+        elif cmd in ['reset', 'r']:  # Setze Pfad zu Downloads zurück
+            working_path = DOWNLOADS_PATH
+
+
+# Startet main()
+if __name__ == '__main__':
+    main()
+
+# os.remove('main.py')
+# os.rmdir('src')
+# shutil.rmtree('venv')
